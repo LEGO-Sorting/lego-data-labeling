19660feeaef5392f5050d2daa74ffb7cb0190de2
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2021-04-24 23:53:41 +0200

    Add generating proper txt file

--- TxtBoundingBox.py
+++ TxtBoundingBox.py
@@ -7,25 +7,17 @@ class TxtBoundingBox:
     xmax = 0
     ymax = 0
     category_name = ""
-    relative_path = ""
-    txt_filename = "actual_training_boxes.csv"
-
-    def __init__(self, relative_path, x_min, y_min, x_max, y_max, category_name):
-        self.xmin = x_min
-        self.ymin = y_min
-        self.xmax = x_max
-        self.ymax = y_max
-        self.relative_path = relative_path
-        self.category_name = category_name
+    absolute_path = ""
+    txt_filename = "actual_training_boxes.txt"
 
     def create_box_line(self):
-        return str(self.xmin+','+self.ymin+','+self.xmax+','+self.ymax)
+        return str(str(self.xmin)+','+str(self.ymin)+','+str(self.xmax)+','+str(self.ymax))
 
     def save_bounding_box(self):
-        output_data_train = open(self.txt_filename, "w")
+        output_data_train = open(self.txt_filename, "a")
         labels = utils.read_label_ids()
         category_id = labels.index(self.category_name)
-        line = str(self.relative_path) + ' ' + self.create_box_line() + ',' + str(category_id)
+        line = str(self.absolute_path) + ' ' + self.create_box_line() + ',' + str(category_id)
         output_data_train.write(line)
         output_data_train.write("\n")
         output_data_train.close()
--- xml_modify.py
+++ xml_modify.py
@@ -1,8 +1,6 @@
 import os
 import utils
 import consts
-import xml.etree.ElementTree as ET
-from pathlib import Path
 
 
 def go_through_files():
@@ -20,36 +18,11 @@ def go_through_files():
                 if str(image).endswith(".xml"):
                     if catalog_number not in available_categories:
                         print("category " + catalog_number + " is not available. Marked as an unknown.")
-                        change_path_files(image)
-                        add_category(image, "unknown")
+                        utils.change_path_files(image)
+                        utils.add_category(image, "unknown")
                         continue
-                    change_path_files(image)
-                    add_category(image, catalog_number)
-
-
-def add_category(xml_image_path, category):
-    tree = ET.parse(xml_image_path)
-    tree_root = tree.getroot()
-    attrib = {}
-    category_element = tree_root.makeelement('category', attrib)
-    category_element.text = category
-
-    for elem in tree_root.iter('category'):
-        elem.text = str(category)
-        tree.write(xml_image_path)
-        return
-    tree_root.append(category_element)
-    tree.write(xml_image_path)
-
-
-def change_path_files(xml_image_path):
-    tree = ET.parse(xml_image_path)
-    tree_root = tree.getroot()
-
-    for elem in tree_root.iter('path'):
-        xml_path = Path(xml_image_path)
-        elem.text = str(xml_path.with_suffix('.jpg'))
-    tree.write(xml_image_path)
+                    utils.change_path_files(image)
+                    utils.add_category(image, catalog_number)
 
 
 if __name__ == '__main__':
