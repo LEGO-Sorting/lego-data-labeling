f91acf8d74e19dc32a0aa048c6a64c6c86c2241e
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2021-04-30 18:18:16 +0200

    Solve after pull

--- consts.py
+++ consts.py
@@ -5,3 +5,4 @@ DATA_CLASSES_FILE = './predefined_classes.txt'
 DATASET_DIR = os.getcwd() + "/TrainYourOwnYOLO/Data/Source_Images/Training_Images/mixed-dataset"
 ACTUAL_DATASET_ROOT_DIR = os.path.join(Path().absolute(), 'TrainYourOwnYOLO', 'Data', 'Source_Images', 'Training_Images',
                                        "actual-dataset-22-04-2021/")
+
